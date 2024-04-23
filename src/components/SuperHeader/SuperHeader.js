0cedaa9110218609be17a15e5a9aad2deea16b56
Pooja Shroff <poojashroff91@gmail.com>
2024-04-22 17:34:06 -0700

    Exercise 1

--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -26,10 +26,16 @@ const Wrapper = styled.div`
   font-size: 0.875rem;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
+  padding: 16px 32px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  gap: 32px;
 `;
 
 const MarketingMessage = styled.span`
   color: ${COLORS.white};
+  margin-right: auto;
 `;
 
 const HelpLink = styled.a`
