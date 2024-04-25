08f50a28e95c4a1671bda155032026240c868d12
Pooja Shroff <poojashroff91@gmail.com>
2024-04-25 10:47:06 -0700

    Exercise 4A

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -56,13 +56,16 @@ const Link = styled.a`
   color: inherit;
 `;
 
-const Wrapper = styled.article``;
+const Wrapper = styled.article`
+margin-bottom: 40px;
+`;
 
 const ImageWrapper = styled.div`
   position: relative;
 `;
 
-const Image = styled.img``;
+const Image = styled.img`
+width: 320px;`;
 
 const Row = styled.div`
   font-size: 1rem;
